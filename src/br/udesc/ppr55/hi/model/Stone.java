07bb79730d6b6ab552f2f6a0c8377aa6074b3ca9
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-05-14 21:13:55 -0300

    Add default constructor

--- src/br/udesc/ppr55/hi/model/Stone.java
+++ src/br/udesc/ppr55/hi/model/Stone.java
@@ -10,6 +10,10 @@ package br.udesc.ppr55.hi.model;
 public class Stone extends Piece {
 
     private int number;
+    
+    public Stone() {
+        super("images/rock.png");
+    }
 
     public Stone(int number) {
         super("images/rock-" + number + ".png");
